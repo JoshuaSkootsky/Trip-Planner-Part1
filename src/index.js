eeb7b8b5b4156062cf5bfb79cb20ee3a9b19bfad
JoshuaSkootsky <joshua.skootsky@gmail.com>
2020-01-27 12:48:41 -0500

    able to view custom icon created with Javascript on local host (not live server)

--- src/index.js
+++ src/index.js
@@ -8,3 +8,19 @@ const map = new mapboxgl.Map({
   zoom: 12, // starting zoom
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
+
+const eleMark = document.createElement("div");
+// mapElement - set width and height
+eleMark.style.width = "32px";
+eleMark.style.height = "39px";
+eleMark.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+
+//new mapboxgl.Marker(YOUR_DOM_ELEMENT).setLngLat(YOUR_COORDS_ARRAY).addTo(map);
+
+// view local: file:///.... Trip-Planner-Part1/public/index.html
+new mapboxgl.Marker(eleMark).setLngLat([-74.009, 40.705]).addTo(map);
+
+/* var marker = new mapboxgl.Marker()
+.setLngLat([30.5, 50.5])
+.addTo(map); */
